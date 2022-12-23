0804b7c0fb10133b58c96e704a436b96e0be9293
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2022-12-23 19:38:36 +0300

    Add new KnifeScript

---
+++ Knife_Hit_Clone/Assets/Scripts/KnifeScript.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeScript : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 throwForce;
+
+    private bool isActive = true;
+
+    private Rigidbody2D rb;
+    private BoxCollider2D knifeCollider;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        knifeCollider= rb.GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isActive) 
+        {
+            rb.AddForce(throwForce, ForceMode2D.Impulse);
+            rb.gravityScale = 1;
+
+        }
+       
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isActive)
+            return;
+
+        isActive= false;
+
+        if(collision.collider.tag == "Log")
+        {
+            rb.velocity = new Vector2(0, 0);
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            this.transform.SetParent(collision.collider.transform);
+
+            knifeCollider.offset = new Vector2(knifeCollider.offset.x, -0.4f);
+            knifeCollider.size = new Vector2(knifeCollider.size.x, 1.2f);
+
+            // TODO Spawn another knife 
+           
+        }
+       
+    }
+}
